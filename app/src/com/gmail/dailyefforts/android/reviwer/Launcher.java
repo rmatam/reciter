92fe2992b0085696bc5fd4eab42ee79e86da92c3
DailyEfforts <DailyEfforts@gmail.com>
2013-04-03 21:28:44 +0800

    update app theme.
    update app theme to android:Theme.Holo.Light.DarkActionBar.

--- app/src/com/gmail/dailyefforts/android/reviwer/Launcher.java
+++ app/src/com/gmail/dailyefforts/android/reviwer/Launcher.java
@@ -234,12 +234,5 @@ public class Launcher extends Activity {
 			return true;
 		}
 	}
-
-	// @Override
-	// public boolean onCreateOptionsMenu(Menu menu) {
-	// // Inflate the menu; this adds items to the action bar if it is present.
-	// getMenuInflater().inflate(R.menu.activity_luncher, menu);
-	// return true;
-	// }
-
+	
 }
