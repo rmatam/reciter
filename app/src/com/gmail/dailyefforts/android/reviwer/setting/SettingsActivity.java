13b623cf139d8c3b811e3c190c250b047932270c
DailyEfforts <DailyEfforts@gmail.com>
2013-04-06 14:08:22 +0800

    add version info in settings activity.

--- app/src/com/gmail/dailyefforts/android/reviwer/setting/SettingsActivity.java
+++ app/src/com/gmail/dailyefforts/android/reviwer/setting/SettingsActivity.java
@@ -2,8 +2,11 @@ package com.gmail.dailyefforts.android.reviwer.setting;
 
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
@@ -34,6 +37,7 @@ public class SettingsActivity extends PreferenceActivity {
 		private String mOptNumSummary;
 		private String mWordCountInOneUnitSummary;
 		private SharedPreferences mSharedPref;
+		private Preference mCurrentVersionPref;
 
 		@Override
 		public void onCreate(Bundle savedInstanceState) {
@@ -44,6 +48,17 @@ public class SettingsActivity extends PreferenceActivity {
 
 			mOptNumListPref = (ListPreference) findPreference(getString(R.string.pref_key_options_count));
 			mWordCountInOneUnitPref = (ListPreference) findPreference(getString(R.string.pref_key_word_count_in_one_unit));
+			mCurrentVersionPref = (Preference) findPreference(getString(R.string.pref_key_current_version));
+			if (mCurrentVersionPref != null) {
+				try {
+					String versionName = getActivity().getPackageManager()
+							.getPackageInfo(getActivity().getPackageName(),
+									PackageManager.GET_SIGNATURES).versionName;
+					mCurrentVersionPref.setSummary("v" + versionName);
+				} catch (NameNotFoundException e) {
+					e.printStackTrace();
+				}
+			}
 
 			mSharedPref = PreferenceManager
 					.getDefaultSharedPreferences(getActivity()
@@ -60,7 +75,7 @@ public class SettingsActivity extends PreferenceActivity {
 				value = Settings.DEFAULT_OPTION_COUNT;
 				mOptNumListPref.setValue(value);
 			}
-			
+
 			value = mWordCountInOneUnitPref.getValue();
 
 			if (value == null && mWordCountInOneUnitPref != null) {
