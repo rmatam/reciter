21d3832eb743b97b415284f428ea79dc6c61020b
DailyEfforts <DailyEfforts@gmail.com>
2013-05-16 01:23:09 +0800

    start to use self-defined xml layout file

--- app/src/com/gmail/dailyefforts/android/reviwer/BookListActivity.java
+++ app/src/com/gmail/dailyefforts/android/reviwer/BookListActivity.java
@@ -18,6 +18,7 @@ public class BookListActivity extends ListActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_book_list);
 
 		String[] books = getResources().getStringArray(R.array.books);
 
